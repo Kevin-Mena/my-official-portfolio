fe3e21392c8bd65aa22537206994396edbff556e
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-15 17:42:21 +0300

    Add hero section animation

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,19 @@
+import AnimatedLetters from "./AnimatedLetters";
 import myResume from "../assets/myResume.pdf";
 
 const Home = () => {
   return (
     <div className="hero">
-      <h5>HELLO, WORLD.</h5>
-      <h1>I'm Kevin Okoth</h1>
+      <h5>
+        <AnimatedLetters text="HELLO, WORLD." />
+      </h5>
+      <h1>
+        <AnimatedLetters text="I'm Kevin Okoth" />
+      </h1>
 
       <p className="intro-position">
         <span>Front-end Developer</span>
-        <span>UI/UX Designer</span>
+        <span>UI/UX Designer"</span>
       </p>
 
       <a href={myResume} download="KevinOkothResume.pdf" className="resume">
