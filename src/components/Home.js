397d1e0fcfc3102e4106f416ba6cb01951ce84a5
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-15 16:03:54 +0300

    Add hero section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div class="hero">
+      <h5>Hi,</h5>
+      <h1>I'm Kevin Okoth</h1>
+
+      <p class="intro-position">
+        <span>Front-end Developer</span>
+        <span>UI/UX Designer</span>
+      </p>
+      <button type="button" class="resume">
+        Get my resume
+      </button>
+    </div>
+  );
 };
 
 export default Home;
