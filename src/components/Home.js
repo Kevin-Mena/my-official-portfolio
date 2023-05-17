a06a80d0ca1a38b2d959ba86be8b76f462a39ae6
Kevin Okoth <kevinokotth04@gmail.com>
2023-05-17 12:38:08 +0300

    Add animations

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,36 @@
+import { motion } from "framer-motion";
 import myResume from "../assets/myResume.pdf";
 
-const Home = () => (
-  <div className="hero">
-    <h5>Hi,</h5>
-    <h1>I'm Kevin</h1>
+const Home = () => {
+  const heroVariants = {
+    hidden: {
+      opacity: 0,
+      y: -100,
+    },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1.5 },
+    },
+  };
 
-    <p className="intro-position">FRONT-END DEVELOPER</p>
+  return (
+    <motion.div
+      className="hero"
+      initial="hidden"
+      animate="visible"
+      variants={heroVariants}
+    >
+      <h5>Hi,</h5>
+      <h1>I'm Kevin</h1>
 
-    <a href={myResume} download="KevinResume.pdf" className="resume">
-      Get my resume
-    </a>
-  </div>
-);
+      <p className="intro-position">FRONT-END DEVELOPER</p>
+
+      <a href={myResume} download="KevinResume.pdf" className="resume">
+        Get my resume
+      </a>
+    </motion.div>
+  );
+};
 
 export default Home;
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { motion, AnimatePresence } from "framer-motion";
+
 const Modal = ({ project, closeModal }) => {
   if (!project) {
     return null;
@@ -5,37 +8,63 @@ const Modal = ({ project, closeModal }) => {
 
   const { id, image, name, source, demo, descr } = project;
 
+  const modalVariants = {
+    hidden: {
+      y: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      y: 0,
+      opacity: 1,
+      transition: { duration: 0.5 },
+    },
+    exit: {
+      y: "100%",
+      opacity: 0,
+      transition: { duration: 0.5 },
+    },
+  };
+
   return (
-    <aside className="modal-overlay" onClick={closeModal}>
-      <div className="modal-container">
-        <button type="button" className="close-button" onClick={closeModal}>
-          &times;
-        </button>
-        <img src={image} alt={name} className="modal-image" />
-        <div className="modal-content" key={id}>
-          <h4>{name}</h4>
-          <p className="project-desc">{descr}</p>
-          <div className="modal-buttons">
-            <a
-              href={demo}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="modal-button"
-            >
-              See Live
-            </a>
-            <a
-              href={source}
-              target="_blank"
-              rel="noreferrer noopener"
-              className="modal-button"
-            >
-              See Source
-            </a>
+    <AnimatePresence>
+      <motion.aside
+        className="modal-overlay"
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        variants={modalVariants}
+        onClick={closeModal}
+      >
+        <div className="modal-container">
+          <button type="button" className="close-button" onClick={closeModal}>
+            &times;
+          </button>
+          <img src={image} alt={name} className="modal-image" />
+          <div className="modal-content" key={id}>
+            <h4>{name}</h4>
+            <p className="project-desc">{descr}</p>
+            <div className="modal-buttons">
+              <a
+                href={demo}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="modal-button"
+              >
+                See Live
+              </a>
+              <a
+                href={source}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="modal-button"
+              >
+                See Source
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </aside>
+      </motion.aside>
+    </AnimatePresence>
   );
 };
 
